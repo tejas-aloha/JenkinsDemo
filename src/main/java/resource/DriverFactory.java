73e65b815c11721c44ed9483e4b8f68a81b7da4d
Tejas <tejsonavane@gmail.com>
2021-05-26 13:12:37 +0530

    Comments Added In Driver Factory

--- src/main/java/resource/DriverFactory.java
+++ src/main/java/resource/DriverFactory.java
@@ -4,37 +4,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class DriverFactory {
-
-
+public class DriverFactory
+{
 	static ThreadLocal<WebDriver> driver= new ThreadLocal<WebDriver>();
 
 	static String browser= System.getProperty("Browser");
 	//static String browser= "Firefox";
 
-
 	public static WebDriver LoadDriver()
 	{
 		switch (browser.toLowerCase()) {
 		case "chrome":
-			System.out.println("Selected Browser is:-"+browser);
+			System.out.println("From Jenkis Selected Browser is:-"+browser);
 			System.setProperty("webdriver.chrome.driver","F:\\Interview Preparation\\Setup\\Drivers\\chromedriver.exe");
 			driver.set(new ChromeDriver());
 			break;
 
 		case "firefox":
-			System.out.println("Selected Browser is:-"+browser);
+			System.out.println("From Jenkis Selected Browser is:-"+browser);
 			System.setProperty("webdriver.gecko.driver","F:\\Interview Preparation\\Setup\\Drivers\\geckodriver.exe");
 			driver.set(new FirefoxDriver());
 			break;
 
 		default:
-			System.out.println("Driver Not Found!");
+			System.out.println("Selected Driver Not Found!");
 			break;
 		}
-
 		return driver.get();
-
 	}
-
 }
